11c06a64e1917ac2cd98f290b7d20ee0c2b4501b
Henke Adolfsson <catears13@gmail.com>
2020-08-05 18:29:55 +0200

    Add simple todo API

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,45 @@
-from flask import Flask
+import jsonschema
+import uuid
+
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
+from loguru import logger
+
+todos = {}
+
+todo_schema = {
+    'type': 'object',
+    'properties': {
+        'id': {'type': 'string'},
+        'text': {'type': 'string'},
+        'completed': {'type': 'boolean'}
+    },
+    'additionalProperties': False
+}
+
+
+def _default_to(json: dict, key: str, value):
+    if not key in json:
+        json[key] = value
+
+def add_todo(json_body) -> str:
+    _default_to(json_body, 'id', str(uuid.uuid4()))
+    _default_to(json_body, 'completed', False)
+    jsonschema.validate(instance=json_body, schema=todo_schema)
+
+    todos[json_body['id']] = json_body
+    return json_body['id']
+
+
+def get_todo(todo_id: str):
+    return todos.get(todo_id)
+
+
+def delete_todo(todo_id: str):
+    if not todo_id in todos:
+        return None
+    del todos[todo_id]
+    return todo_id
 
 
 class HelloWorld(Resource):
@@ -10,6 +50,38 @@ class HelloWorld(Resource):
     def get(self):
         message = self.message.split(' ')
         return message
+
+
+class TodoAll(Resource):
+
+    def get(self):
+        keys = todos.keys()
+        return list(keys)
+
+    def post(self):
+        return add_todo(request.get_json()), 201
+
+
+class TodoSingle(Resource):
+
+    def get(self, todo_id: str):
+        logger.info(f'Returning TODO "{todo_id}"')
+        x = get_todo(todo_id)
+        if x is None:
+            return abort(404)
+        return x, 200
+
+    def post(self, todo_id: str):
+        body = request.get_json()
+        body['id'] = todo_id
+        todo_id = add_todo(body)
+        return todo_id, 201
+
+    def delete(self, todo_id: str):
+        res = delete_todo(todo_id)
+        if res is None:
+            return abort(404)
+        return res, 202
 
 
 def create_routes(app: Flask) -> Flask:
@@ -23,6 +95,10 @@ def create_routes(app: Flask) -> Flask:
             'message': app.config.message
         }
     )
+
+    api.add_resource(TodoAll, '/todo')
+    api.add_resource(TodoSingle, '/todo/<string:todo_id>')
+
     app.logger.info('Registered "/hello"')
 
     return app
