e55e987ae0cef5aa1fbb6467a5de57769c18ff36
Henke Adolfsson <catears13@gmail.com>
2020-08-07 09:22:33 +0200

    Implement renaming of repo in bootstrapper

--- dodo.py
+++ dodo.py
@@ -1,4 +1,18 @@
+import glob
+import os
+
 STDOUT = 2
+
+def replace_in_files(fpath_pattern: str, target: str, replacement: str):
+    for fpath in glob.glob(fpath_pattern, recursive=True):
+        replace_in_file(fpath, target, replacement)
+
+def replace_in_file(fpath: str, target: str, replacement: str):
+    with open(fpath, 'r') as f:
+        content = f.read()
+    content = content.replace(target, replacement)
+    with open(fpath, 'w') as f:
+        f.write(content)
 
 
 def task_test():
@@ -13,7 +27,21 @@ def task_test():
 def task_bootstrap():
 
     def rename(name: str):
+        if '-' in name:
+            raise AttributeError('Name cannot have dashes in it!')
         print(f'Renaming repository to "{name}"')
+        original_name = 'src'
+        new_name = name
+        replace_in_file('./docker-compose.yml',
+                        './{0}:/app/{0}'.format(original_name),
+                        './{0}:/app/{0}'.format(new_name))
+        replace_in_file('./pyproject.toml',
+                        'flask-rest-api-template',
+                        new_name)
+        replace_in_files('./tests/**/*.py',
+                         'from {} import'.format(original_name),
+                         'from {} import'.format(new_name))
+        os.rename(original_name, new_name)
 
     return {
         'actions': [
