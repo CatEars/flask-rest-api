470d353c3490873df3008dcde9930b3822fdfa4b
Henke Adolfsson <catears13@gmail.com>
2020-08-09 08:13:25 +0200

    Add section that is stripped out once bootstrapping is complete

--- dodo.py
+++ dodo.py
@@ -3,18 +3,6 @@ import os
 
 STDOUT = 2
 
-def replace_in_files(fpath_pattern: str, target: str, replacement: str):
-    for fpath in glob.glob(fpath_pattern, recursive=True):
-        replace_in_file(fpath, target, replacement)
-
-def replace_in_file(fpath: str, target: str, replacement: str):
-    with open(fpath, 'r') as f:
-        content = f.read()
-    content = content.replace(target, replacement)
-    with open(fpath, 'w') as f:
-        f.write(content)
-
-
 def task_test():
     return {
         'actions': [
@@ -33,11 +21,46 @@ def task_coverage():
         'verbosity': STDOUT
     }
 
+
+### Bootstrap-only-section start
+
+def replace_in_files(fpath_pattern: str, target: str, replacement: str):
+    for fpath in glob.glob(fpath_pattern, recursive=True):
+        replace_in_file(fpath, target, replacement)
+
+def replace_in_file(fpath: str, target: str, replacement: str):
+    with open(fpath, 'r') as f:
+        content = f.read()
+    content = content.replace(target, replacement)
+    with open(fpath, 'w') as f:
+        f.write(content)
+
+
+def remove_section(fpath: str, section_name: str):
+    with open(fpath, 'r') as f:
+        lines = f.readlines()
+    start_pattern = f'### {section_name} start'.lower()
+    end_pattern = f'### {section_name} end'.lower()
+    kept_lines = []
+    in_section = False
+    for line in lines:
+        if line.lower().startswith(start_pattern):
+            in_section = True
+        elif line.lower().startswith(end_pattern):
+            in_section = False
+        elif not in_section: # never include section delimiters
+            kept_lines.append(line)
+    with open(fpath, 'w') as f:
+        f.writelines(kept_lines)
+
+
 def task_bootstrap():
 
     def rename(name: str):
         if '-' in name:
             raise AttributeError('Name cannot have dashes in it!')
+        if not name:
+            raise AttributeError('Needs to supply --name [name]')
         print(f'Renaming repository to "{name}"')
         original_name = 'src'
         new_name = name
@@ -52,11 +75,15 @@ def task_bootstrap():
                          'from {} import'.format(new_name))
         os.rename(original_name, new_name)
 
+    def update_dodofile():
+        remove_section('./dodo.py', 'Bootstrap-only-section')
+
     return {
         'actions': [
             ['cp', 'artifacts/settings.toml', '.'],
             ['cp', 'artifacts/.secrets.toml', '.'],
-            (rename, )
+            (rename, ),
+            (update_dodofile, )
         ],
         'params': [
             {
@@ -67,3 +94,5 @@ def task_bootstrap():
         ],
         'verbosity': STDOUT
     }
+
+### Bootstrap-only-section end
